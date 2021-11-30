ac284490c91b1d1aa5a6fee98b592782fbf8479a
Aritra12344 <83257965+Aritra12344@users.noreply.github.com>
2021-11-30 11:20:55 +0600

    Add files via upload

---
+++ form.js
@@ -0,0 +1,32 @@
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA-ZGz0GkdeyULipkALJbb0D2U85cBt1cc",
+    authDomain: "login-d0713.firebaseapp.com",
+    projectId: "login-d0713",
+    storageBucket: "login-d0713.appspot.com",
+    messagingSenderId: "441912840000",
+    appId: "1:441912840000:web:519478706b51463365f4a5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+	const auth = firebase.auth();
+
+	
+
+	
+
+	function signUp(){
+
+		
+
+	
+alert("Signed Up");
+
+	}
+
+	
+
+
